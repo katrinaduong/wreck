37477512e51c49942ece4fc2fc17d4723f1b5071
Katrina Duong <katrinaduong@ucla.edu>
2018-11-29 23:11:28 -0800

    Added dynamic error checking for empty message field

--- js/issues.js
+++ js/issues.js
@@ -15,6 +15,17 @@ $('#nevermind, .close-button').click(function() {
   $('body').toggleClass("adding-issue");
 });
 
+$("#new-issue-text").on('input',function(e){
+  if(e.target.value === ''){
+    // Textarea has no value
+  } else {
+    // Textarea has a value
+    if (!$('#help-text').is(':hidden')) {
+      $('#help-text').toggle();
+    }
+  }
+});
+
 $('#post').click(function() {
   if (newWelcome == true) {
     $('#new-welcome').toggle();
@@ -22,27 +33,27 @@ $('#post').click(function() {
   }
   var date = getCurrentTime()
   var text = $("#new-issue-text").val()
-  var newPost =
-  `<div class="issue">
-    <div class="issue-date">${date}</div>
-    <label class="issue-content">${text}</label>
-    <hr>
-    <div class="issue-footer">
-      <div class="issue-reply issue-button">
-        <i class="fa fa-reply"></i>
-        Reply
+  if (text.length < 1) {
+    $('#help-text').toggle();
+  } else {
+    var newPost =
+    `<div class="issue">
+      <div class="issue-date">${date}</div>
+      <label class="issue-content">${text}</label>
+      <hr>
+      <div class="issue-footer">
+        <div class="issue-react-angry issue-button">
+          <button onclick="increment(this)" value="0">
+            React 😠 <label class="angry-count"></label>
+          </button>
+        </div>
       </div>
-      <div class="issue-react-angry issue-button">
-        <button onclick="increment(this)" value="0">
-          React 😠 <label class="angry-count"></label>
-        </button>
-      </div>
-    </div>
-  </div>`
+    </div>`
 
-  $('#issues-container').prepend(newPost)
-  $('#new-issue-text').val("")
-  $('body').toggleClass("adding-issue")
+    $('#issues-container').prepend(newPost)
+    $('#new-issue-text').val("")
+    $('body').toggleClass("adding-issue")
+  }
 });
 
 function increment(react) {
