0e4dbf258e1df487052f31e9fae4297cf195ff05
Hannah Elarabawy <helarabawy@ucla.edu>
2018-12-05 22:12:35 -0800

    minor changes

--- js/issues.js
+++ js/issues.js
@@ -48,6 +48,13 @@ $("#new-issue-text").on('input',function(e){
   }
 });
 
+$('.close-issue').click(function(e) {
+  console.log("CLOSING ISSUE");
+  // e.preventDefault();
+  // console.log($(this).parent().parent().attr('class'));
+  // $(this).parent().parent().remove();
+});
+
 $('#post').click(function() {
   var date = getCurrentTime()
   var text = $("#new-issue-text").val()
@@ -58,7 +65,10 @@ $('#post').click(function() {
   } else {
     var newPost =
     `<div class="issue">
-      <div class="issue-date">${date}</div>
+      <div class="issue-date">
+        <i class="fa fa-times w3-xxlarge close-issue"></i>
+        ${date}
+      </div>
       <label class="issue-content">${text}</label>
       <hr>
       <div class="issue-footer">
